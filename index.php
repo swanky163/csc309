dadc276205bc8b20dfd071321fd405bd9953c16f
swanky163 <85730535+swanky163@users.noreply.github.com>
2023-07-17 22:20:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,19 +7,56 @@
     <h2>Registration Form</h2>
 
     <!-- Select all users and display in a Table -->
-    <table>
+    <table cellpadding="10" cellspacing="1" border="1">
         <thead>
-            <!-- Complete the table header row -->
+            <tr>
+                <td>SN</td>
+                <td>Last Name</td> 	
+                <td>First Name</td> 	
+                <td>Gender</td>
+                <td>Date of Birth</td> 	
+                <td>Email</td>
+            </tr>
         </thead>
 
         <tbody>
-            <?php
-                // Complete code to:
-                // Connect to the DB
-                // Select all records from users table
-                // Display all the selected records
-            ?>
+           <?php
+                //my server variables
+                $server = 'localhost'; // 127.0.0.1
+                $username = 'root';
+                $password = '';
+                $db = 'csc309';
+
+                //my server connection
+                $conn = new mysqli($server, $username, $password, $db);
+            
+            //check server connection
+            if ($conn){
+                //my select query
+                $sql = "SELECT * FROM users";
+                
+                //query result
+                $result = $conn->query($sql);
+                
+                //fetch the data from the result set
+                while ($row = $result->fetch_assoc()) {?>
+                    <tr>
+                        <td><?php echo $row["id"];?></td>
+                        <td><?php echo $row["lastname"];?></td> 	
+                        <td><?php echo $row["firstname"];?></td> 	
+                        <td><?php echo $row["gender"];?></td>
+                        <td><?php echo $row["date_of_birth"];?></td> 	
+                        <td><?php echo $row["email"];?></td>
+                   </tr>
+                
+                 <?php }
+                
+                }
+                // Close connection
+                $conn->close();
+             ?>
+            
         </tbody>
     </table>
-</body>
+  </body>
 </html>
